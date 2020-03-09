94719699e75f4fb693faff30bfc679aa74ec6b13
dmacpalle <52783923+dmacpalle@users.noreply.github.com>
2020-03-09 12:54:26 +0100

    Add files via upload

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,52 +1,82 @@
+// we'll always need this line at the beginning
 PennController.ResetPrefix(null);
-PennController.AddHost("http://files.lab.florianschwarz.net/ibexfiles/PennController/SampleTrials/");
+
+// Start your script
+PennController(
+    
+    newText("Welcome", "Welcome to the experiment!")
+        .print() 
+    , 
+    newText("press", "Please press <i>Continue</i> to begin.")
+        .print()
+    .settings.italic()
+    ,
+        newButton("welcome", "Continue")
+        .print()
+    .settings.bold()
+    .wait()
+    
+    
+   );
 
 PennController(
-    newButton("test sentence", "Ready?") // Task 1
-    .print()
-    .wait()
+    
+    newText("sentence1", "William Shakespeare has three children.")// a new text element named 'sentence' 
+        .print() // we need this line to print the element
+    , // we ALWAYS!!! need a comma between elements
+    newText("question", "<br>Is this sentence coherent?")
+        .print()
+    .settings.italic()
     ,
-    newAudio("test sentence", "fishRound.ogg")
-    .play() // Immediately play the audio file
+        newText("instruction", "<br>Press 'F' for yes, 'J' for no")
+        .print()
+    .settings.bold()
     ,
-    newImage("competitor", "1fishSquareTank.png")// An image with 1 fish that swims in a square tank
-    .settings.size(200,200)    
-    .print()       
-    ,
-    newImage("target", "2fishRoundTank.png")// An image with 2 fish that swim in a round tank
-    .settings.size(200,200)    
-    .print()        
-    ,
-    newCanvas("tanks", 500, 200)
-    .settings.add(   50, 0, getImage("competitor") ) // moved 50px from the left limit
-    .settings.add( 270, 0, getImage("target") )  // 20px from the right limit of 'competitor' ('competitor' right edge = (50px from the left) + (200px wide) =  250px)
-    .print()
-    ,
-    newSelector("tank") // We indicate that target+competitor belong to a selection group
-    .settings.add( getImage("target") , getImage("competitor") )
-    .settings.once() // Task 4
-    .wait()
-    ,
-    getAudio("test sentence")
-    .wait() // Task 3
-    // .stop() // Task 2
-);
+    newKey("response1", "FJ") //  a new key element called 'response'; accepts responses as key press 'F' (coherent) or 'J' (incoherent)
+        .settings.log()
+        .wait() // wait for a key press before validation (important!)
 
-/* CHANGES
-- remove the text under each image
+    
+   );
+
+PennController(
+    
+    newText("sentence2", "Angela Merkel has blue eyes.")// a new text element named 'sentence' 
+        .print() // we need this line to print the element
+    , // we ALWAYS!!! need a comma between elements
+    newText("question", "<br>Is this sentence coherent?")
+        .print()
+    .settings.italic()
+    ,
+        newText("instruction", "<br>Press 'F' for yes, 'J' for no")
+        .print()
+    .settings.bold()
+    ,
+    newKey("response2", "FJ") //  a new key element called 'response'; accepts responses as key press 'F' (coherent) or 'J' (incoherent)
+        .settings.log()
+        .wait() // wait for a key press before validation (important!)
+    
+   );
 
 
-TASKS
+
+/*   TASKS
+
+
+
 Tip: make sure to test the experiment after each change! This way, if it doesn't work you've only changed one thing and know where the problem is
 
-1. Add a 'start' button, which must be pressed before the trial starts.
+1. Add a welcome screen, that says 'Welcome to the experiment!'
+    - tell participants to click 'Continue' when they're ready to begin
+    - Use 'newButton' to add a button element labelled 'Continue', which waits until it is clicked before continuing to the next screen
 
-2. Stop the audio once a selection has been made (FYI: this will make the change from task 2 useless)
+3. Add a second trial that is the same as the first, but with the sentence 'Angela Merkel has blue eyes'
 
-3. Wait for the audio to finish before ending trial
+Run through the experiment twice, and then look at the 'results' file (press 'Refresh' before opening it!).
 
-4. Only one selection is possible (i.e., can't change your mind, even though the pictures remain on the screen)
+4. use '.settings.log()' to log the newKey selection to the results file
 
+Run through the experiment again two more times, and then look at the 'results' file (press 'Refresh' before opening it!).
 
 
 
