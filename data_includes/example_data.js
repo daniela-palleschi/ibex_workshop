46e9f22b01cfe5e89b696024e4c4db64b0fab35e
dmacpalle <52783923+dmacpalle@users.noreply.github.com>
2020-03-10 15:23:14 +0100

    Add files via upload

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,83 +1,28 @@
-// we'll always need this line at the beginning
-PennController.ResetPrefix(null);
+PennController.ResetPrefix(null); // Initiates PennController
 
-// Start your script
+
+// Start typing your code here
+
+
 PennController(
-    
-    newText("Welcome", "Welcome to the experiment!")
-        .print() 
-    , 
-    newText("press", "Please press <i>Continue</i> to begin.")
-        .print()
-    .settings.italic()
+    defaultText
+    .settings.css("font-size", "30")
     ,
-        newButton("welcome", "Continue")
-        .print()
-    .settings.bold()
+    newText("sentence1", "Colourless <b>green</b> ideas sleep furiously.")
+    .settings.italic()
+    .print()
+    ,
+    newKey("response", "FJ")
+    .settings.log()
     .wait()
-    
-    
-   );
-
-PennController(
-    
-    newText("sentence1", "Colourless green ideas sleep furiously.")// a new text element named 'sentence' 
-        .print() // we need this line to print the element
-    , // we ALWAYS!!! need a comma between elements
-    newText("question", "<br>Is this sentence coherent?")
-        .print()
-    .settings.italic()
     ,
-        newText("instruction", "<br>Press 'F' for yes, 'J' for no")
-        .print()
-    .settings.bold()
+    getText("sentence1")
+    .remove()
     ,
-    newKey("response1", "FJ") //  a new key element called 'response'; accepts responses as key press 'F' (coherent) or 'J' (incoherent)
-        .settings.log()
-        .wait() // wait for a key press before validation (important!)
-
-    
-   );
-
-PennController(
-    
-    newText("sentence2", "Odourless beige concepts slumber angrily.")// a new text element named 'sentence' 
-        .print() // we need this line to print the element
-    , // we ALWAYS!!! need a comma between elements
-    newText("question", "<br>Is this sentence coherent?")
-        .print()
-    .settings.italic()
+    newButton("continue", "Continue")
+    .print()
+    .settings.center()
     ,
-        newText("instruction", "<br>Press 'F' for yes, 'J' for no")
-        .print()
-    .settings.bold()
-    ,
-    newKey("response2", "FJ") //  a new key element called 'response'; accepts responses as key press 'F' (coherent) or 'J' (incoherent)
-        .settings.log()
-        .wait() // wait for a key press before validation (important!)
-    
-   );
-
-
-
-/*   TASKS
-
-
-
-Tip: make sure to test the experiment after each change! This way, if it doesn't work you've only changed one thing and know where the problem is
-
-1. Add a welcome screen, that says 'Welcome to the experiment!'
-    - tell participants to click 'Continue' when they're ready to begin
-    - Use 'newButton' to add a button element labelled 'Continue', which waits until it is clicked before continuing to the next screen
-
-3. Add a second trial that is the same as the first, but with the sentence 'Odourless beige concepts slumber angrily'
-
-Run through the experiment twice, and then look at the 'results' file (press 'Refresh' before opening it!).
-
-4. use '.settings.log()' to log the newKey selection to the results file
-
-Run through the experiment again two more times, and then look at the 'results' file (press 'Refresh' before opening it!).
-
-
-
-*/
+    newKey("continue"," ")
+    .wait()
+);
