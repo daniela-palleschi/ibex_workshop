e79c59166c320f35d73c4cda1cacc85644180f73
dmacpalle <52783923+dmacpalle@users.noreply.github.com>
2020-04-07 13:39:22 +0200

    Add files via upload

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,5 +1,44 @@
 PennController.ResetPrefix(null);  // leave this here
 
-
-// Start typing your code here
-
+PennController("Trial1",
+               defaultText
+               .settings.css("font-size", "30")
+               ,
+               newText("sentence1", "Colourless <b>green</b> ideas sleep furiously.")
+               .settings.italic()
+               .print()
+               ,
+               newKey("response", "FJ")
+               .wait()
+               .settings.log()
+               ,
+               getText("sentence1")
+               .remove()
+               ,
+               newButton("continue", "Continue")
+               .print()
+               .settings.center()
+               .wait()
+               .settings.log()
+              );
+PennController("Trial2",
+               defaultText
+               .settings.css("font-size", "30")
+               ,
+               newText("sentence1", "Odourless beige memories slumber angrily.")
+               .settings.italic()
+               .print()
+               ,
+               newKey("response", "FJ")
+               .wait()
+               .settings.log()
+               ,
+               getText("sentence1")
+               .remove()
+               ,
+               newButton("continue", "Continue")
+               .print()
+               .settings.center()
+               .settings.log()
+               .wait()
+              );
