23ec0a6329cc37504df6c6810085262fe8e5a5c4
dmacpalle <52783923+dmacpalle@users.noreply.github.com>
2020-04-14 19:14:24 +0200

    Add files via upload

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,5 +1,18 @@
-PennController.ResetPrefix(null);  // leave this here
+PennController.ResetPrefix(null);
 
-
-// Start typing your code here
-
+PennController(
+    newText("test sentence", "The fish swim in a tank which is <i>perfectly round</i>")  // italics
+    .settings.center()    // centred
+    .print()
+    ,
+    newImage("competitor", "https://amor.cms.hu-berlin.de/~pallesid/workshop_2/1fishSquareTank.png")// An image with 1 fish that swims in a square tank
+    .print()       
+    ,
+    newImage("target", "https://amor.cms.hu-berlin.de/~pallesid/workshop_2/2fishRoundTank.png")// An image with 2 fish that swim in a round tank
+    .print()        
+    ,
+    newSelector("tank") // We indicate that target+competitor belong to a selection group
+    .settings.add( getImage("target") , getImage("competitor") )
+    .settings.log()
+    .wait()         // wait until target or competitor is selected 
+);
