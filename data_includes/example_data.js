13c9e65eb158f58788848bf0b612f04e2dc15bda
dmacpalle <52783923+dmacpalle@users.noreply.github.com>
2020-04-14 19:53:08 +0200

    Add files via upload

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,5 +1,27 @@
-PennController.ResetPrefix(null);  // leave this here
+PennController.ResetPrefix(null);
+PennController.AddHost("https://amor.cms.hu-berlin.de/~pallesid/workshop_2/");
 
-
-// Start typing your code here
-
+PennController(
+    newText("test sentence", "The fish swim in a tank which is <i>perfectly round</i>")  // italics
+    .settings.center()    // centred
+    .print()
+    ,
+    newImage("competitor", "1fishSquareTank.png")// An image with 1 fish that swims in a square tank
+    .settings.size(200,200)    
+    .print()       
+    ,
+    newImage("target", "2fishRoundTank.png")// An image with 2 fish that swim in a round tank
+    .settings.size(200,200)    
+    .print()        
+    ,
+    newCanvas("tanks", 500, 200)
+    .settings.add(   50, 0, getImage("competitor") ) // moved 50px from the left limit
+    .settings.add( 270, 0, getImage("target") )  // 20px from the right limit of 'competitor' ('competitor' right edge = (50px from the left) + (200px wide) =  250px)
+    .print()
+    ,
+newSelector()
+    .settings.add( getImage("competitor") , getImage("target") )
+    .settings.keys(          "F"    ,          "J"   )
+    .settings.log()
+    .wait()
+   );
